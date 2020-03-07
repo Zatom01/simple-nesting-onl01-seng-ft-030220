7aeef6a509f8f08adef9820ef20e58b282e32f2d
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 06:57:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,24 +40,24 @@ def alan_kay_is_known_for
      programmer_hash[:alan_kay][:known_for]
 end
 
-# def dennis_ritchies_language
-# 	programmer_hash = 
-# 		{
-#         :grace_hopper => {
-#           :known_for => "COBOL",
-#           :languages => ["COBOL", "FORTRAN"]
-#         },
-#         :alan_kay => {
-#           :known_for => "Object Orientation",
-#           :languages => ["Smalltalk", "LISP"]
-#         },
-#         :dennis_ritchie => {
-#           :known_for => "Unix",
-#           :languages => ["C"]
-#         }
-#     }
-#     programmer_hash[:grace_hopper][:dennis_ritchie][:languages][0]
-# end
+def dennis_ritchies_language
+	programmer_hash = 
+		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+    }
+    programmer_hash[:grace_hopper][:dennis_ritchie][:languages][0]
+end
 
 # def adding_matz
 # # add the following information to the top level of programmer_hash
